1b489304bbb7ae36a9e2ee9fbcc38901c70739f0
tamotsu ichiba <ichibat@gmail.com>
2020-05-27 22:06:46 +0900

    after lesson7 with thumbnail

--- src/components/Booklist.jsx
+++ src/components/Booklist.jsx
@@ -12,7 +12,7 @@ const Booklist = props => {
         {
           bookData === null
             ? <p>now loading...</p>
-        : bookData.data.items.map((x, index) => <li key={index}>{x.volumeInfo.title}{' '}{x.volumeInfo.authors}<br></br><img src={x.volumeInfo.imageLinks.thumbnail} /></li>)
+        : bookData.data.items.map((x, index) => <li key={index}>{x.volumeInfo.title}{' '}{x.volumeInfo.authors}<br></br><img src={ x.volumeInfo.readingModes.image ? x.volumeInfo.imageLinks.thumbnail : 'no-image' } /></li>)
         }
       </ul>
     </div>
